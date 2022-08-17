cf5c6f88249d8cd40eb2e4fd325a9ecc7c088d46
BenPot <pot.94.handig@gmail.com>
2022-08-17 16:46:09 +0800

    Testing

--- inc/components/top.php
+++ inc/components/top.php
@@ -3,7 +3,7 @@
 ?>
 
 <div style="padding-top: 50px;">
-    <div style="display: flex;">
-        <div><img src="<?php echo LOGO; ?>"/></div>
+    <div style="display: flex; justify-content: flex-end;">
+        <div><div style="height: 50px; width: 50px; border: 5px solid green;">LOGO</div></div>
     </div>
 </div>
