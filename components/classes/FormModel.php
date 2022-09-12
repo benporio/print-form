ad0df32c31516032067ef1d848bc751ee840189f
Ben <pot.94.handig@gmail.com>
2022-09-12 21:27:13 +0800

    Progress

--- components/classes/FormModel.php
+++ components/classes/FormModel.php
@@ -22,7 +22,7 @@ class FormModel extends DataModel
     {
         ### Setting default logo setup
         $this->logo = new class {
-            public string $path = INCLUDE_DIR.'/logo.jpg';
+            public string $path = INCLUDE_DIR.'/images/logo.jpg';
             public string $height = '90px';
             public string $width = '130px';
             public string $alignment = 'center';
